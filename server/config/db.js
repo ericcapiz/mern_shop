6e2641f34fcab8d788456801b4cf28d8d89a812a
ericcapiz <ericcapiz@gmail.com>
2021-05-29 16:09:48 -0500

    update

--- server/config/db.js
+++ server/config/db.js
@@ -1,11 +1,13 @@
-require('dotenv').config();
 const mongoose = require('mongoose');
+require('dotenv').config();
 
 const connectDB = async () =>{
     try {
         await mongoose.connect(process.env.MONGO_URI,{
             useNewUrlParser: true,
-            useUnifiedTopology:true
+            useUnifiedTopology:true,
+            useUnifiedTopology: true,
+            useCreateIndex: true,
         });
         console.log("MongoDB Connection Success");
     } catch (error) {
--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const express = require('express');
+const path = require('path');
 const cors = require('cors');
 const connectDB = require('./config/db');
 const productRoutes = require('./routes/productRoutes');
@@ -12,6 +13,17 @@ app.use(express.json());
 app.use(cors());
 
 app.use('/api/products', productRoutes);
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname, '../client/build')))
+
+    app.get('*', (req,res)=>{
+        res.sendFile(__dirname, "client", "build", "index.html");
+    })
+}else{
+    app.get('/', (req,res) =>{
+        res.send("api up")
+    })
+}
 
 const PORT = process.env.PORT || 5000;
 
